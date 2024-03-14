b5fdc0e6bdc398530369b829618762735280fcfb
semaanursenol <semaanurdogaan@gmail.com>
2024-03-15 00:03:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Bu benim uygulamam")
+
+
+// node modules klasörü yer kapladığı için yada yanlıslıkla silinirse npm install diyerek dependensies içinde yazan paketler otomatik indirilir.
+
+// sadece geliştirme ortamında ihtiyacımız olan paketler devDependencies olarak kaydedilir
